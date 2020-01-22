78f2b79f30857ee35cd364c2eb56fc70f5362cbd
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 21:41:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 2 
+    return array.join(' and ')
+  elsif array.size > 1
+    last_element = array.pop
+    return "#{array.join(', ')}" + ", and #{last_element}"
+  else
+    return array.join(', ')
+  end
 end
