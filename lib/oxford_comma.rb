cfb51aabb7ed2c5c863b40f205083d1844b501ab
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 23:57:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  tostring = ""
+  lastelement = array.pop
+  if array.length == 0
+    tostring = lastelement
+  elsif array.length == 1
+    tostring = "#{array[0]} and #{lastelement}"
+  else
+    tostring = array.join(", ")
+    tostring += ", and "+lastelement
+  end
+  return tostring
 end
