ea523f2bb28dc608d2d425bd77252802e4a87c79
Steven Perfors <stevecanbuild@gmail.com>
2020-01-25 21:56:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.count == 1 
+    return "#{array[0]}"
+  elsif array.count == 2
+    return array.join(" and ")
+  elsif array.count >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
+
+#oxford_comma["fiddleheads", "okra", "kohlrabi"]
