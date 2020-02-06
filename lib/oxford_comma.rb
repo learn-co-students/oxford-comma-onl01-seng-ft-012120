ffc696dc6be458b118dd6b0621b839da7fe7cb14
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-06 16:37:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-array = { "fork","spoon", "plate","table" }
-array.join(!!!)
-end
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
